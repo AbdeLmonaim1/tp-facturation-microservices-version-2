ab6e52237e82bb727aab7ee555886158bf636630
AbdeLmonaim1 <abdelmonaimahdoud3@gmail.com>
2025-10-25 14:41:09 +0100

    implemented api gateway dynamically using DiscoveryClientRouteDefinitionLocator Bean

--- api-gateway-service/src/main/java/ma/enset/apigatewayservice/ApiGatewayServiceApplication.java
+++ api-gateway-service/src/main/java/ma/enset/apigatewayservice/ApiGatewayServiceApplication.java
@@ -2,10 +2,22 @@ package ma.enset.apigatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ApiGatewayServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayServiceApplication.class, args);
+    }
+    //For configurate gateway dynamically we will use this bean
+    @Bean
+    DiscoveryClientRouteDefinitionLocator locator(
+            ReactiveDiscoveryClient rdc,
+            DiscoveryLocatorProperties dlp
+    ){
+        return new DiscoveryClientRouteDefinitionLocator(rdc, dlp);
     }
 }
