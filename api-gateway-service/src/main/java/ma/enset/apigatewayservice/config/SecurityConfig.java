61e718fa746227e978c484da4a544402e55710de
AbdeLmonaim1 <abdelmonaimahdoud3@gmail.com>
2025-11-09 17:40:11 +0100

    feat(docker): containerize all microservices and Keycloak setup Added Docker configuration for all microservices and integrated Keycloak container for authentication management.

--- api-gateway-service/src/main/java/ma/enset/apigatewayservice/config/SecurityConfig.java
+++ api-gateway-service/src/main/java/ma/enset/apigatewayservice/config/SecurityConfig.java
@@ -36,28 +36,20 @@ public class SecurityConfig {
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration corsConfig = new CorsConfiguration();
-
         // Origines autorisées
         corsConfig.setAllowedOrigins(List.of("http://localhost:4200"));
-
         // Méthodes HTTP autorisées
         corsConfig.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
-
         // Headers autorisés
         corsConfig.setAllowedHeaders(Arrays.asList("*"));
-
         // Permettre les credentials
         corsConfig.setAllowCredentials(true);
-
         // Headers exposés au client
         corsConfig.setExposedHeaders(Arrays.asList("Authorization"));
-
         // Durée de cache
         corsConfig.setMaxAge(3600L);
-
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfig);
-
         return new CorsWebFilter(source);
     }
 }
