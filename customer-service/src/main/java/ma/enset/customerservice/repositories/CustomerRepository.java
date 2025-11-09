70f5dac311c88e9283119ef1cdbf2e7a1e54e0ee
AbdeLmonaim1 <abdelmonaimahdoud3@gmail.com>
2025-11-09 03:44:02 +0100

    st test unitaire with JUnit5

--- customer-service/src/main/java/ma/enset/customerservice/repositories/CustomerRepository.java
+++ customer-service/src/main/java/ma/enset/customerservice/repositories/CustomerRepository.java
@@ -3,5 +3,10 @@ package ma.enset.customerservice.repositories;
 import ma.enset.customerservice.entities.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByEmail(String email);
+    List<Customer> findByNameContainingIgnoreCase(String keyword);
 }
--- customer-service/src/test/java/ma/enset/customerservice/repositories/CustomerRepositoryTest.java
+++ customer-service/src/test/java/ma/enset/customerservice/repositories/CustomerRepositoryTest.java
@@ -1,7 +1,41 @@
 package ma.enset.customerservice.repositories;
 
+import ma.enset.customerservice.entities.Customer;
+import org.assertj.core.api.AssertionsForClassTypes;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
-
+//Inject all the spring data jpa things
+@DataJpaTest
 class CustomerRepositoryTest {
+    @Autowired
+    private CustomerRepository customerRepository;
 
+    //Before each test save a customer in the database
+    @BeforeEach
+    public void setUp() {
+        customerRepository.save(Customer.builder()
+                        .name("Monaim")
+                        .email("monaim1@gmail.com")
+                .build());
+        customerRepository.save(Customer.builder()
+                .name("Amine")
+                .email("amine@gmail.com")
+                .build());
+        customerRepository.save(Customer.builder()
+                .name("Mouad")
+                .email("mouad@gmail.com")
+                .build());
+    }
+    @Test
+    public void shouldFindCustomerByEmail() {
+        String givenEmail = "monaim1@gmail.com";
+        Optional<Customer> result = customerRepository.findByEmail(givenEmail);
+        AssertionsForClassTypes.assertThat(result).isPresent();
+    }
 }
