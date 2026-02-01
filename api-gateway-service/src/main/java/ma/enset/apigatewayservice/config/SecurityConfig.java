ff812dd9a41ae818d28697d8796ca2ee0cbbb304
AbdeLmonaim1 <abdelmonaimahdoud3@gmail.com>
2026-02-01 18:59:51 +0100

    resolves CORS problems in gateway pattern

--- api-gateway-service/src/main/java/ma/enset/apigatewayservice/config/SecurityConfig.java
+++ api-gateway-service/src/main/java/ma/enset/apigatewayservice/config/SecurityConfig.java
@@ -1,31 +1,95 @@
 package ma.enset.apigatewayservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.*;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+
+
+import java.util.Arrays;
 
 
 @Configuration
 @EnableWebFluxSecurity
 public class SecurityConfig {
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration config = new CorsConfiguration();
+//        config.setAllowedOrigins(Arrays.asList("http://localhost")); // your frontend origin
+//        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+//        config.setAllowedHeaders(Arrays.asList("*"));
+//        config.setAllowCredentials(true);
+//        return new UrlBasedCorsConfigurationSource() {{
+//            registerCorsConfiguration("/**", config);
+//        }};
+//    }
+//    @Bean
+//    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+//        return http
+//                .cors(Customizer.withDefaults())
+//                .csrf(cr->cr.disable())
+//                .authorizeExchange(exchange -> exchange
+//                        .pathMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+//                        .anyExchange().authenticated()
+//                )
+//                .oauth2ResourceServer(oauth2 -> oauth2
+//                        .jwt(Customizer.withDefaults())
+//                )
+//                .build();
+//
+//    }
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private String issuerUri;
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.jwk-set-uri}")
+    private String jwkSetUri;
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(Arrays.asList("http://localhost"));
+        config.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(Arrays.asList("*"));
+        config.setAllowCredentials(true);
+        return new UrlBasedCorsConfigurationSource() {{
+            registerCorsConfiguration("/**", config);
+        }};
+    }
+
+    @Bean
+    public ReactiveJwtDecoder jwtDecoder() {
+        NimbusReactiveJwtDecoder decoder = NimbusReactiveJwtDecoder
+                .withJwkSetUri(jwkSetUri)
+                .build();
+
+        decoder.setJwtValidator(JwtValidators.createDefaultWithIssuer(issuerUri));
+
+        return decoder;
+    }
+
     @Bean
     public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
         return http
-                .cors(co -> co.disable())
-                .csrf(cr->cr.disable())
+                .cors(Customizer.withDefaults())
+                .csrf(cr -> cr.disable())
                 .authorizeExchange(exchange -> exchange
                         .pathMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                         .anyExchange().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(Customizer.withDefaults())
+                        .jwt(jwt -> jwt.jwtDecoder(jwtDecoder()))
                 )
                 .build();
-
     }
 
 }
